ee9e6d1693fefd747e7837b5065179546b994350
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-27 14:59:36 +0000

    sanity check on performanceData event

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import {useState,useEffect} from 'react'
+import styles from '../styles/Home.module.css';
+import socket from "../utils/socketConn"
 export default function Home() {
+  const [perfData,setPerfData]=useState({});
+  useEffect(()=>{
+    socket.on('perfData',(data)=>{
+      console.log(data)
+    })
+  },[])
   return (
     <div className={styles.container}>
-     
+     <ul>
+       <li>
+       </li>
+     </ul>
     </div>
   )
 }
