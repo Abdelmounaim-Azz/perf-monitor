7b68e57de8c9cb8ef13e0f5015cc8d43db4abb71
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-27 05:55:00 +0000

    listen for perfData event on the server

--- server/mainSio.js
+++ server/mainSio.js
@@ -1,4 +1,7 @@
 const mainSio=(io,socket)=>{
-  console.log('main Socket Here!',socket.id)
+  // console.log('main Socket Here!',socket.id)
+  socket.on('perfData',(data)=>{
+    console.log(data)
+  })
 };
 module.exports=mainSio;
