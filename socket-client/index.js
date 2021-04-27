42b67644c6c667a7023f1699858c2293ed154d73
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-27 05:19:40 +0000

    request connection to socket-io server

--- socket-client/index.js
+++ socket-client/index.js
@@ -1,7 +1,12 @@
 //node js program that gets performance data using os module and send it to the socket server.
 const os =require('os');
 const io=require("socket.io-client");
-let socket=io('http://localhost:8080')
+let socket=io('http://localhost:8080');
+
+
+socket.on('connect',()=>{
+  console.log("I am connected to the server")
+})
 
 function performanceData(){
   return new Promise(async (resolve, reject)=>{
