4378c47b2cf98f1f3ac1b20772ed8dc84c8eed4e
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-28 10:34:59 +0000

    map through machine section and display components

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,21 +1,19 @@
-import {useState,useEffect} from 'react'
-import styles from '../styles/Home.module.css';
-import socket from "../utils/socketConn"
+import {useState, useEffect} from "react";
+import styles from "../styles/Home.module.css";
+import socket from "../utils/socketConn";
+import Section from "../components/Section";
 export default function Home() {
-  const [perfData,setPerfData]=useState({});
-  let sections=[];
-  useEffect(()=>{
-    socket.on('data',(data)=>{
-      const currData={...perfData};
-      currData[data.macAdress]=data;
+  const [perfData, setPerfData] = useState({});
+  let sections = [];
+  Object.entries(perfData).forEach(([key, value]) => {
+    sections.push(<Section key={key} data={value} />);
+  });
+  useEffect(() => {
+    socket.on("data", (data) => {
+      const currData = {...perfData};
+      currData[data.macAdress] = data;
       setPerfData(currData);
-    })
-  },[data])
-  return (
-    <div className={styles.container}>
-     <ul>
-       
-     </ul>
-    </div>
-  )
+    });
+  }, [data]);
+  return <div className={styles.container}>{sections}</div>;
 }
