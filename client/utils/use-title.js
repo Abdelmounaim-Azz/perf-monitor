675bf04fc087867d322f433c3be7aace860802c6
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-29 04:28:01 +0000

    use-title helper

--- client/utils/use-title.js
+++ client/utils/use-title.js
@@ -9,7 +9,7 @@ export function Title({title}) {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <meta name="description" content="Monitor your machine performance." />
       </Head>
